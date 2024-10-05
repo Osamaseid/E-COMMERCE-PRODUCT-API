7e257c60c1e52760c567e1b5daffb47022bb8305
osama seid <osaseid1@gmail.com>
2024-10-05 14:13:14 +0300

    some change

--- Eommerce/settings.py
+++ Eommerce/settings.py
@@ -78,16 +78,25 @@ WSGI_APPLICATION = 'Eommerce.wsgi.application'
 
 
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',  # 
+   #     'NAME': 'ecommerce',           #
+    #    'USER': 'root',                
+     #   'PASSWORD': '0924475152@os',          
+      #  'HOST': 'localhost',                    
+       # 'PORT': '3306',                         
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 
-        'NAME': 'ecommerce',           #
-        'USER': 'root',                
-        'PASSWORD': '0924475152@os',          
-        'HOST': 'localhost',                    
-        'PORT': '3306',                         
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
